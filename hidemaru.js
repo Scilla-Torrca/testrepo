80c8ce821ef23eff73309954db5385ffe8b02ed6
Scilla-Torrca <torrca@torrca.com>
2023-03-22 12:24:50 +0900

    :sparkles: hidemaru super hage (hidemaru.js)

--- hidemaru.js
+++ hidemaru.js
@@ -3,4 +3,5 @@ const Torrca = "fusafusa";
 if(Hidemaru == "hage" | Torrca == "fusafusa") {
     console.log("お前以外ふさふさ。ふさふさじゃないのはお前だけ。")
     console.log("ふさふさ以外ありえない");
+    const hage = "imifumei";
 }
