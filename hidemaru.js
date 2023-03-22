6d438d188fa65e7b59d9b7a78ab0d32cd8ab7fe9
Scilla-Torrca <torrca@torrca.com>
2023-03-22 12:10:36 +0900

    :sparkles: はげ (hidemaru.js)

--- hidemaru.js
+++ hidemaru.js
@@ -2,4 +2,5 @@ const Hidemaru = "hage";
 const Torrca = "fusafusa";
 if(Hidemaru == "hage" | Torrca == "fusafusa") {
     console.log("お前以外ふさふさ。ふさふさじゃないのはお前だけ。")
+    console.log("ふさふさいがいありえない");
 }
